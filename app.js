7b31683fcb7d61f1f5dcc1b27292b4af62e16fd4
skathanas <32923297+skathanas@users.noreply.github.com>
2020-01-24 10:37:58 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ document.querySelector('.get-jokes').addEventListener('click', getJokes);
 function getJokes(event){
 
     const userNumber = document.querySelector('input[type = "number"]').value;
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
     //get request
-    fetch(`http://api.icndb.com/jokes/random/${userNumber}`)
+    fetch(`${proxyUrl}http://api.icndb.com/jokes/random/${userNumber}`)
     .then(function(response){
 
         console.log(response);
